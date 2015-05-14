44f947ca5edd4b000f9067238e71fb376e282408
paturikiran <kiranthecoder@gmail.com>
2015-05-14 13:14:12 +0530

    Directives explanation

---
+++ VehicleInventory/app/controllers/addressRegistrationController.js
@@ -0,0 +1,9 @@
+﻿app.controller('addressRegistrationController', ['$scope',
+    function ($scope) {
+        $scope.address = {
+            city: "New York",
+            state: "NY",
+            zip: "12345",
+            street: "James Street 1234"
+        };
+    }]);
---
+++ VehicleInventory/app/directives/custom-register.js
@@ -0,0 +1,56 @@
+﻿app.directive('customRegisterDirective', [function () {
+    return {
+        restrict: "A",
+        // template:"<label>First Name</label>"
+        templateUrl: 'app/templates/register-address.html',
+        //controller: function  ($scope) {
+        //    console.log("hello World");
+        //}
+        controller: "addressRegistrationController",
+        link: function (scope, element,attrs) {
+            var ele = element.find('button');
+            console.log(attrs);
+            ele.bind('click', function () {
+                console.log("i am clicked");
+            });
+           
+        }
+
+    };
+
+}]);
+
+
+app.directive('parentDirective', function () {
+    return {
+        restrict: "A",
+        template: "<label>{{address.city}}</label>" +
+                "<div child-directive></div>",
+        link: {
+            pre:function(scope,elem,attr){
+                //console.log(scope);
+                console.log(elem);
+            },
+            post: function(scope,elem,attr){
+                //console.log("iam the parent post link");
+            }    
+        }
+        
+
+    }
+});
+app.directive('childDirective', function () {
+    return {
+        restrict: "A",
+        template: "<label>{{address.zip}}</label>",
+        link: { pre:function(scope,elem,attr){
+            //console.log(scope);
+            console.log(elem);
+        },
+            post: function(scope,elem,attr){
+               // console.log("iam the child post link");
+            }    
+        }
+        
+    }
+});
